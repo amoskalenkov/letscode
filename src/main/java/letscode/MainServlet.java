7b9d03075ec0f14a864d95ae0e36fc532c264f23
Aleksandr Moskalenkov <amk.upsale@gmail.com>
2020-07-30 00:02:11 +0300

    JspBasics

--- src/main/java/letscode/MainServlet.java
+++ src/main/java/letscode/MainServlet.java
@@ -7,7 +7,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @WebServlet(urlPatterns = {"/cool-servlet", "/my-cool-servlet/*"})
